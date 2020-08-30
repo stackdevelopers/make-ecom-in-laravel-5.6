5571296e9b85b2052538e2ae7a54a3aa43eb6ef0
Stack Developers <expertphpdevelopers@gmail.com>
2020-08-30 17:30:06 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,18 @@
+-- To get the Database, Please join Stack Developers Youtube Channel as Premium Member with below link :-
+-- https://www.youtube.com/channel/UCExO2i-tLU1NyVZD6zOJQlw/join
+
+-- Steps to download Database after becoming Premium Member :-
+-- Steps to download Basic E-commerce Website in Laravel 5.6 from Part 1 to 57 along with database  :-
+
+/*
+1) Open below website :-
+http://sitemakers.in
+2) Click on "STACK DEVELOPERS" link at top navigation that will take you to Stack Developers section
+3) Click on "Login to Download" link to download Basic E-commerce Series in Laravel 5.6 (Part 1 to 57)
+4) Enter Username and Password if you have already got it otherwise get one by joining Stack Developers Youtube Channel as Premium Member with below link :-
+https://www.youtube.com/channel/UCExO2i-tLU1NyVZD6zOJQlw/join
+(Username/Password will be emailed to you / shared at Social network / Channel Community only for Premium Members)
+5) After login, click on "Click Here to Download" link to download Basic E-commerce Series in Laravel 5.6 (Part 1 to 57).
+This time code will get downloaded in zipped format along with database.
+All the Best :)
+*/
